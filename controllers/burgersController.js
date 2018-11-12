5a241251185023982c651f8a60a39e31829b22d1
cumareshan27 <cumareshan@yahoo.com>
2018-11-11 19:52:56 -0500

    fxed the DB errors

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -9,7 +9,7 @@ var burger = require("../models/burger.js");
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
-      burger: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
@@ -32,7 +32,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  devoured.update({
+  burger.update({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,19 +2,19 @@
 $(function() {
     $(".change-devoured").on("click", function(event) {
       var id = $(this).data("id");
-      var newdevoured = $(this).data("newnewdevoured");
+      var newdevoured = $(this).data("newdevoured");
   
-      var newnewdevouredState = {
-        devoured: newnewdevoured
+      var newdevouredState = {
+        devoured: newdevoured
       };
   
       // Send the PUT request.
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
-        data: newnewdevouredState
+        data: newdevouredState
       }).then(
         function() {
-          console.log("changed devoured to", newnewdevoured);
+          console.log("changed devoured to", newdevoured);
           // Reload the page to get the updated list
           location.reload();
         }
@@ -25,15 +25,15 @@ $(function() {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
-      var newCat = {
-        name: $("#bu").val().trim(),
-        sleepy: $("[name=devoured]:checked").val().trim()
+      var newBurger = {
+        burger_name: $("#bu").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
       };
   
       // Send the POST request.
       $.ajax("/api/burgers", {
         type: "POST",
-        data: newCat
+        data: newBurger
       }).then(
         function() {
           console.log("created new burger");
